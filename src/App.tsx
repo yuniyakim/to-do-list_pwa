4e1456a271afbf66a39f82580ef361ba7aa14592
Yuniya Kim <yuniyakim@gmail.com>
2024-05-19 13:55:54 +0300

    Add onRender

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import {useState} from 'react';
+// import {useState} from 'react';
+import {useState, Profiler} from 'react';
 import {ToDo} from './types/interfaces';
 import {ToDoList} from './components/to-do-list/to-do-list';
 import {ToDoForm} from './components/to-do-form/to-do-form';
@@ -13,11 +14,30 @@ export const App = () => {
     return newToDos;
   };
 
+  const onRender = (_id: string, _phase: 'mount' | 'update', actualDuration: number, _baseDuration: number, _startTime: number, _commitTime: number) => {
+    // ios
+    // const platform = 'ios';
+    // const url = 'http://127.0.0.1:5555/add';
+    // android
+    const platform = 'macos';
+    const url = 'http://192.168.67.1:5555/add';
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({app: 'pwa', platform: platform, time: actualDuration, device: 'Apple MacBook Air'})
+    });
+  }
+
   return (
-    <div className="toDo-list-app-container">
-      <div className="toDo-list-app-title">ToDo List</div>
-      <ToDoForm addToDo={addToDo} />
-      <ToDoList toDos={toDos} setToDos={setToDos} />
-    </div>
+    <Profiler id="App" onRender={onRender}>
+      <div className="toDo-list-app-container">
+        <div id="toDo-list-app-title" className="toDo-list-app-title">ToDo List</div>
+        <ToDoForm addToDo={addToDo} />
+        <ToDoList toDos={toDos} setToDos={setToDos} />
+      </div>
+    </Profiler>
   );
 };
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import {defineConfig} from 'vite';
 import {VitePWA} from 'vite-plugin-pwa';
 import react from '@vitejs/plugin-react';
+// import mkcert from "vite-plugin-mkcert";
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const profiling = isProduction && {
+  "react-dom/client": "react-dom/profiling",
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    // mkcert(),
     VitePWA({
       registerType: 'autoUpdate',
       workbox: {
@@ -13,4 +21,22 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      ...profiling,
+      // 'react-dom': path.resolve(
+      //   __dirname,
+      //   'node_modules/react-dom/profiling'
+      // ),
+      // 'scheduler/tracing': path.resolve(
+      //     __dirname,
+      //     'node_modules/scheduler/tracing-profiling'
+      // ),
+    },
+    
+  },
+  server: {
+    host: '0.0.0.0',
+    // https: true
+  }
 });
